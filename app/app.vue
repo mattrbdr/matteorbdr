452df23e449ef2776555278bfb445d78589ff143
mattrbdr <mribardiere@outlook.com>
2026-05-15 12:45:37 +0200

    fix: add baseURL prefix to favicon path

--- app/app.vue
+++ app/app.vue
@@ -12,7 +12,7 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { key: 'theme-color', name: 'theme-color', content: color }
   ],
-  link: [{ rel: 'icon', href: '/favicon.ico' }]
+  link: [{ rel: 'icon', href: '/matteorbdr/favicon.ico' }]
 })
 
 useSeoMeta({
