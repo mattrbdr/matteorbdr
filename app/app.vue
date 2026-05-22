bc2cde4380ce56dcda7f6ae6c62e41f94ad73057
mattrbdr <mribardiere@outlook.com>
2026-05-22 14:16:41 +0200

    feat: add google-site-verification meta tag

--- app/app.vue
+++ app/app.vue
@@ -1,33 +1,41 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
-const { navLinks } = useNavLinks()
+const colorMode = useColorMode();
+const { navLinks } = useNavLinks();
 
 const color = computed(() =>
-  colorMode.value === 'dark' ? '#020618' : 'white'
-)
-const { app: { baseURL } } = useRuntimeConfig()
+  colorMode.value === "dark" ? "#020618" : "white",
+);
+const {
+  app: { baseURL },
+} = useRuntimeConfig();
 
 useHead({
   meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color }
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { key: "theme-color", name: "theme-color", content: color },
+    {
+      name: "google-site-verification",
+      content: "wnmiyGVdyjMSM2G-RkwwStqdIrQ3c5HxkLtG58xaenk",
+    },
   ],
-  link: [{ rel: 'icon', type: 'image/x-icon', href: `${baseURL}favicon.ico` }]
-})
+  link: [{ rel: "icon", type: "image/x-icon", href: `${baseURL}favicon.ico` }],
+});
 
 useSeoMeta({
-  titleTemplate: '%s - Portfolio',
-  twitterCard: 'summary_large_image'
-})
+  titleTemplate: "%s - Portfolio",
+  twitterCard: "summary_large_image",
+});
 
-const { data: files } = useLazyAsyncData('search-files', () =>
-  Promise.all([queryCollectionSearchSections('blog')]).then(data => data.flat())
-)
+const { data: files } = useLazyAsyncData("search-files", () =>
+  Promise.all([queryCollectionSearchSections("blog")]).then((data) =>
+    data.flat(),
+  ),
+);
 
-const { data: navigation } = useLazyAsyncData('search-nav', () =>
-  Promise.all([queryCollectionNavigation('blog')]).then(data => data.flat())
-)
+const { data: navigation } = useLazyAsyncData("search-nav", () =>
+  Promise.all([queryCollectionNavigation("blog")]).then((data) => data.flat()),
+);
 </script>
 
 <template>
